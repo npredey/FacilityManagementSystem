b8ac135d6dd2da8917b85088c72c309af0a322b8
nickpredey <npredey@luc.edu>
2017-02-17 20:44:43 -0600

    Created structural interfaces and classes

---
+++ FacilityManagementSystem/src/com/OfficeBuilding/FacilityUse/IFacilityUse.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.OfficeBuilding.FacilityUse;
+
+/**
+ *
+ * @author nickpredey
+ */
+public interface IFacilityUse {
+
+    public void getUsageRate();
+
+    public void getActualUsage();
+
+    public void getInspections();
+
+    public void vacateFacility();
+
+    public void addUserToFacility();
+}
