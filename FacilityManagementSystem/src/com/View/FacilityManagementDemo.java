545bb0fc1b4fa110db8598dcfd944329afbdbaca
nickpredey <npredey@luc.edu>
2017-02-19 17:15:30 -0600

    More updating of tests

--- FacilityManagementSystem/src/com/View/FacilityManagementDemo.java
+++ FacilityManagementSystem/src/com/View/FacilityManagementDemo.java
@@ -21,6 +21,8 @@ public class FacilityManagementDemo {
     public static int testOperatingBudget1 = 50000;
     public static int testSavings2 = 150000;
     public static int testOperatingBudget2 = 55000;
+    public static int testSavings3 = 1000;
+    public static int testOperatingBudget3 = 5000;
 
     /*Constants Related to Location*/
     public static String testStreetName = "Chicago Avenue";
@@ -36,6 +38,7 @@ public class FacilityManagementDemo {
     /*Constants related to Units*/
     public static int capacityUnit1 = 100;
     public static int capacityUnit2 = 150;
+    public static int capacityUnit3 = 300;
 
     public static void main(String[] args) {
         MaintenanceLog maintenanceLog = new MaintenanceLog();
@@ -45,19 +48,24 @@ public class FacilityManagementDemo {
         FacilityBudget facilityBudgetBuilding = new FacilityBudget(testSavings0, testOperatingBudget0);
         FacilityBudget facilityBudgetUnit1 = new FacilityBudget(testSavings1, testOperatingBudget1);
         FacilityBudget facilityBudgetUnit2 = new FacilityBudget(testSavings2, testOperatingBudget2);
+        FacilityBudget facilityBudgetNewUnit = new FacilityBudget(testSavings3, testOperatingBudget3);
 
         FacilityDetail detailDemoBuilding = new FacilityDetail(0, -1, facilityBudgetBuilding, facilityLocation);
         FacilityDetail detailDemoUnit1 = new FacilityDetail(1, capacityUnit1, facilityBudgetUnit1, facilityLocation);
         FacilityDetail detailDemoUnit2 = new FacilityDetail(2, capacityUnit2, facilityBudgetUnit2, facilityLocation);
+        FacilityDetail detailTestNewUnit = new FacilityDetail(3, capacityUnit3, facilityBudgetNewUnit, facilityLocation);
 
         Unit[] units = {new Unit(detailDemoUnit1), new Unit(detailDemoUnit2)};
         IFacility facility = new Building(units);
         facility.addNewDetail(detailDemoBuilding);
+        System.out.println("Initial capacity (should be 250): " + facility.getCapacity());
 
         IFacilityDomain staff = new Staff(testStaffName);
         IFacilityUse facilityUse = new FacilityUse("9:00:00", "19:00:00", facility);
 
-        System.out.println(facility.getCapacity());
+        IFacility newUnit = new Unit(detailTestNewUnit);
+        facility.addFacility(newUnit);
+        System.out.println("Capacity after new unit (should be 550): " + facility.getCapacity());
         System.out.println("-----------------------DONE-----------------------");
         //create a Facility Domain
         //create an inspector object
